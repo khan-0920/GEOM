9a7f6605c3bf2143559e15e85ee2e7f049453a67
khan-0920 <khan@stu.pku.edu.cn>
2025-02-22 19:06:36 +0800

    code changed

--- utils/dataset.py
+++ utils/dataset.py
@@ -1,11 +1,87 @@
 import pickle
 import os
 import torch
-from torch_geometric.data import Data 
+import numpy as np
+from torch_geometric.data import Dataset, Data
 
 from rdkit import Chem
 
-allowable_features = {
+class Featurizer:
+    def __init__(self, allowable_sets):
+        self.dim = 0
+        self.features_mapping = {}
+        for k, s in allowable_sets.items():
+            #s = sorted(list(s))
+            self.features_mapping[k] = dict(zip(s, range(self.dim, len(s) + self.dim)))
+            self.dim += len(s)
+
+    def encode(self, inputs):
+        output = np.zeros((self.dim,))
+        for name_feature, feature_mapping in self.features_mapping.items():
+            feature = getattr(self, name_feature)(inputs)
+            if feature not in feature_mapping:
+                continue
+            output[feature_mapping[feature]] = 1.0
+        return output
+
+
+class AtomFeaturizer(Featurizer):
+    def __init__(self, allowable_sets):
+        super().__init__(allowable_sets)
+
+    def possible_atomic_num_list(self, atom):
+        return atom.GetAtomicNum()
+
+    def possible_chirality_list(self, atom):
+        return atom.GetChiralTag()
+
+    def possible_degree_list(self, atom):
+        return atom.GetTotalDegree()
+
+    def possible_formal_charge_list(self, atom):
+        return atom.GetFormalCharge()
+    
+    def possible_numH_list(self, atom):
+        return atom.GetTotalNumHs()
+    
+    def possible_number_radical_e_list(self, atom):
+        return atom.GetNumRadicalElectrons()
+    
+    def possible_hybridization_list(self, atom):
+        return atom.GetHybridization().name.lower()
+    
+    def possible_is_aromatic_list(self, atom):
+        return atom.GetIsAromatic()
+    
+    def possible_is_in_ring_list(self, atom):
+        return atom.IsInRing()
+
+
+class BondFeaturizer(Featurizer):
+    def __init__(self, allowable_sets):
+        super().__init__(allowable_sets)
+        self.dim += 1
+
+    def encode(self, bond):
+        output = np.zeros((self.dim,))
+        if bond is None:
+            output[-1] = 1.0
+            return output
+        output = super().encode(bond)
+        return output
+
+    def possible_bond_type_list(self, bond):
+        return str(bond.GetBondType())
+
+    def possible_bond_stereo_list(self, bond):
+        return str(bond.GetStereo())
+    
+    def possible_is_conjugated_list(self, bond):
+        return bond.GetIsConjugated()
+
+
+atom_featurizer = AtomFeaturizer(
+    allowable_sets={
     "possible_atomic_num_list": list(range(1, 119)) + ["misc"],
     "possible_chirality_list": [
         "CHI_UNSPECIFIED",
@@ -20,11 +96,16 @@ allowable_features = {
     "possible_formal_charge_list": [-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, "misc"],
     "possible_numH_list": [0, 1, 2, 3, 4, 5, 6, 7, 8, "misc"],
     "possible_number_radical_e_list": [0, 1, 2, 3, 4, "misc"],
-    "possible_hybridization_list": ["SP", "SP2", "SP3", "SP3D", "SP3D2", "misc"],
+    "possible_hybridization_list": ["sp", "sp2", "sp3", "sp3d", "sp3d2", "misc"],
     "possible_is_aromatic_list": [False, True],
-    "possible_is_in_ring_list": [False, True],
-    "possible_bond_type_list": ["SINGLE", "DOUBLE", "TRIPLE", "AROMATIC", "misc"],
-    "possible_bond_stereo_list": [
+    "possible_is_in_ring_list": [False, True]
+    }
+)
+
+bond_featurizer = BondFeaturizer(
+    allowable_sets={
+        "possible_bond_type_list": ["SINGLE", "DOUBLE", "TRIPLE", "AROMATIC", "misc"],
+        "possible_bond_stereo_list": [
         "STEREONONE",
         "STEREOZ",
         "STEREOE",
@@ -33,39 +114,49 @@ allowable_features = {
         "STEREOANY",
     ],
     "possible_is_conjugated_list": [False, True],
-}
+    }
+)
 
-
-def rd_to_data(molecule_dataset,conf_id,):
+def rd_to_data(conf_id,data):
     """
+    
+    conf_id: the conformation id of the data
+    
     data
-    x: [atom_types,...]
+    x: [atom_feature,...]
     edge_index: [2, num_edges]
     pos: [num_nodes, 3]
     edge_attr: [num_edges, features]
     y: boltzman distribution
     
     """
-    mol = data['conformers'][1]['rd_mol']
+    mol = data['conformers'][conf_id]['rd_mol']
     
-    x = torch.tensor([atom.GetIdx() for atom in mol.GetAtoms()])
+    x = torch.tensor([atom_featurizer.encode(atom) for atom in mol.GetAtoms()])
     
     pos = torch.tensor(mol.GetConformer().GetPositions())
     
-    row, col, edge_type = [], [], []
+    edge, edge_attr = [], []
     
     for bond in mol.GetBonds():
         
         start, end = bond.GetBeginAtomIdx(), bond.GetEndAtomIdx()
-        row += [start, end]
-        col += [end, start]
-        edge_type += [bond.GetBondType()]
+        edge += [start,end]
+        edge += [end,start]
+        edge_attr += [bond_featurizer.encode(bond)]
+        edge_attr += [bond_featurizer.encode(bond)]
     
-    edge_index = torch.tensor(row+col,dtype=torch.long)
+    edge_index = torch.tensor(edge,dtype=torch.long)
     
+    y = data['conformers'][conf_id]['boltzmannweight']
     
+    output = Data(x=x, 
+                  edge_index=edge_index, 
+                  edge_attr=edge_attr, 
+                  pos=pos, 
+                  )
     
-    return NotImplementedError
+    return output
 
 
 if __name__ == "__main__":
